ac886301053627033ca29e12787d9f04a9e79224
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-05 14:36:24 +0100

    Refactoring

--- main.c
+++ main.c
@@ -1,63 +1,9 @@
 #include "../libctf/src/libctf.h"
 
+#include "statistics.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-
-int 
-minimum (int values[], unsigned int size)
-{
-	int m = values[0];
-
-	for (unsigned int i = 0; i < size; i++)
-		if (values[i] < m)
-			m = values[i];
-
-	return m;
-}
-
-int
-maximum (int values[], unsigned int size)
-{
-	int m = values[0];
-
-	for (unsigned int i = 0; i < size; i++)
-		if (values[i] > m)
-			m = values[i];
-
-	return m;
-}
-
-int
-compare (const void* a, const void* b)
-{
-	return (*(int*)a - *(int*)b);
-}
-
-float
-median (int values[], unsigned int size)
-{
-	int* values_sorted;
-	values_sorted = malloc(sizeof(int) * size);
-	
-	memcpy(values_sorted, values, sizeof(int) * size);
-	qsort(values_sorted, size, sizeof(int), compare);
-
-	if (size % 2)
-		return (float)values_sorted[size/2];
-	else
-		return (float)(values_sorted[size/2 - 1] + values_sorted[size/2]) / 2.0f;
-}
-
-float
-arithmetic_mean (int values[], unsigned int size)
-{
-	int sum = 0;
-
-	for (unsigned int i = 0; i < size; i++)
-		sum += values[i];
-	
-	return (float)sum/(float)size;
-}
 
 void
 usage ()
---
+++ statistics.h
@@ -0,0 +1,17 @@
+#ifndef CTF_STATS_STATISTICS_H
+#define CTF_STATS_STATISTICS_H
+
+#endif
+
+int 
+minimum (int values[], unsigned int size);
+
+int
+maximum (int values[], unsigned int size);
+
+float
+median (int values[], unsigned int size);
+
+float
+arithmetic_mean (int values[], unsigned int size);
+
