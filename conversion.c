2b3788c5d0db59bbf91dea46394ec9ffe41275ea
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-07-06 07:12:47 +0100

    String conversion utilitiy

---
+++ conversion.c
@@ -0,0 +1,29 @@
+#include "conversion.h"
+
+const char*
+kind_to_string (uint8_t kind)
+{
+	const char *translation_table[] = 
+	{
+		"none", 
+		"int", 
+		"float", 
+		"pointer",
+		"array",
+		"function", 
+		"struct", 
+		"union", 
+		"enum", 
+		"forward declaration", 
+		"typedef",
+		"volatile",
+		"const", 
+		"restrict"
+	};
+
+	if (kind <= CTF_KIND_MAX && kind >= 0)
+		return translation_table[kind];
+	else
+		return "unresolvable";
+}
+
---
+++ conversion.h
@@ -0,0 +1,14 @@
+#ifndef CTF_STATS_CONVERSION_H
+#define CTF_STATS_CONVERSION_H
+
+#include "../libctf/src/libctf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char*
+kind_to_string (uint8_t kind);
+
+#endif
+
